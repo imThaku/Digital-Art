76c27956ad8468b6ddc675d35c092f865d376dde
Hugo Lefebvre <hugo.lefebvre@epitech.eu>
2020-02-24 15:40:58 +0100

    Added all user data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {Hero} from './hero';
 import {Classe} from './classe.enum';
+import * as Surnames from './../assets/surnames.json';
+import * as Names from './../assets/name.json';
 
 @Component({
   selector: 'app-root',
@@ -14,22 +16,59 @@ export class AppComponent {
     let identifier: string;
     let array: string[];
     let result: number[] = [];
-    let user: Hero = {classe: '', lvl: 0, name: ''};
     console.log(data.target.value);
     identifier = data.target.value;
     array = identifier.split(/\s/);
-    console.log(array);
-    let sum = 0;
-    for (const str of array) {
-      sum = 0;
-      for (let i = 0; i < str.length; i++) {
-        sum += str.charCodeAt(i);
+
+    if(array.length == 4){
+      console.log(array);
+      let sum = 0;
+      for (const str of array) {
+        sum = 0;
+        for (let i = 0; i < str.length; i++) {
+          sum += str.charCodeAt(i);
+        }
+        result.push(sum);
       }
-      result.push(sum);
+    } else{
+      let length = identifier.length;
+      let dividedLength = length / 4;
+      let indexStart = 0;
+      let indexEnd = dividedLength;
+      let sum = 0;
+      for(let i = 0; i < 4; i++){
+        let currentString = identifier.substr(indexStart, indexEnd);
+        sum = 0;
+        for (let i = 0; i < currentString.length; i++) {
+          sum += currentString.charCodeAt(i);
+        }
+        result.push(sum);
+        indexStart += dividedLength;
+        indexEnd += dividedLength;
+        console.log(result)
+      }
     }
-    let var1 = (result[0].valueOf() / 10 ) - Math.floor((result[0].valueOf() / 10 ));
-    var1 = var1 * 10;
-    user.classe = Classe[Math.round(var1)];
-    console.log(user.classe);
+
+    let surnames = Surnames.surnames;
+    let names = Names.names;
+    let classe = (result[0].valueOf() / 10 ) - Math.floor((result[0].valueOf() / 10 ));
+    console.log(classe);
+    let surname = (result[1].valueOf() / 100 ) - Math.floor((result[1].valueOf() / 100 ));
+    let name = (result[2].valueOf() / 100 ) - Math.floor((result[2].valueOf() / 100 ));
+    let lvl = (result[3].valueOf() / 100 ) - Math.floor((result[3].valueOf() / 100 ));
+    classe = classe * 10;
+    surname = surname * 100;
+    name = name * 100;
+    lvl = lvl * 100;
+
+    let user: Hero = {
+      classe: Classe[Math.round(classe)], 
+      surname: surnames[Math.round(surname)].name,
+      description: surnames[Math.round(surname)].description,
+      name: names[Math.round(name)].name,
+      lvl: Math.round(lvl)
+    }
+    console.log(user);
+
   }
 }
