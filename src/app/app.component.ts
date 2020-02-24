f18d43dac477ebf6aead18a8e092f03da5452ae0
Stephane <ssantos@norsys.fr>
2020-02-24 14:03:22 +0100

    Change local variable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,27 +9,27 @@ import {Classe} from './classe.enum';
 })
 export class AppComponent {
   title = 'digital-art';
-  private identifier: string;
-  private array: string[];
-  private result: number[] = [];
-  private user: Hero = {classe: '', lvl: 0, name: ''};
 
   test(data: any) {
+    let identifier: string;
+    let array: string[];
+    let result: number[] = [];
+    let user: Hero = {classe: '', lvl: 0, name: ''};
     console.log(data.target.value);
-    this.identifier = data.target.value;
-    this.array = this.identifier.split(/\s/);
-    console.log(this.array);
+    identifier = data.target.value;
+    array = identifier.split(/\s/);
+    console.log(array);
     let sum = 0;
-    for (const str of this.array) {
+    for (const str of array) {
       sum = 0;
       for (let i = 0; i < str.length; i++) {
         sum += str.charCodeAt(i);
       }
-      this.result.push(sum);
+      result.push(sum);
     }
-    let var1 = (this.result[0].valueOf() / 10 ) - Math.floor((this.result[0].valueOf() / 10 ));
+    let var1 = (result[0].valueOf() / 10 ) - Math.floor((result[0].valueOf() / 10 ));
     var1 = var1 * 10;
-    this.user.classe = Classe[Math.round(var1)];
-    console.log(this.user.classe);
+    user.classe = Classe[Math.round(var1)];
+    console.log(user.classe);
   }
 }
